188b92fe3f804e753818ab34ec18416919e88388
Xhakayoub <mouissi.ayoub@gmail.com>
2020-03-18 14:06:19 +0100

    function for all data

--- script.py
+++ script.py
@@ -12,10 +12,13 @@ from pyvirtualdisplay import Display
 
 app = Flask(__name__)
 
+types = ['stats', 'passing', 'shooting', 'playingtime', 'keepers', 'keepersadv', 'misc']
+competitions = [9, 8, 12, 13, 11, 19, 20] 
+
 def get_league_by_number(argument): 
     switcher = { 
-        8: "Champions-League-Stats",
         13: "Ligue-1-Stats", 
+        8: "Champions-League-Stats",     
         11: "Serie-A-Stats",
         19: "Europa-League-Stats",
         20: "Bundesliga-Stats",
@@ -27,7 +30,7 @@ def get_league_by_number(argument):
     # value of passed argument if it is present  
     # in dictionary otherwise second argument will 
     # be assigned as default value of passed argument 
-    return switcher.get(argument, "nothing") 
+    return switcher.get(argument) 
 
 
 @app.route('/<int:numberOfLeague>/<string:types>')
@@ -54,5 +57,46 @@ def get_data_by_league(numberOfLeague, types):
     browser.close()
     browser.quit()
     return response
+
+@app.route('/all')
+def get_all_data():
+    data = {}
+    for competition in competitions:
+        for typ in types:
+            league = get_league_by_number(competition)
+            chrome_options = Options()
+            chrome_options.headless = True
+            # chrome_options.add_argument("--headless")
+            # chrome_options.add_argument('--disable-gpu')  # Last I checked this was necessary.
+            browser = webdriver.Chrome('C:\\Users\\mouis\\webDriver\\chromedriver', options=chrome_options)
+            tosh = 'le nombre : {}'
+            tosh = tosh.format(competition)
+            print(tosh)
+            print('le nombre : '+league)
+            
+            link = 'https://fbref.com/en/comps/{}/'+typ+'/'+league
+            link = link.format(competition)
+            print(link)
+            browser.get(link)
+            if typ == 'stats': typ = 'standard'
+            if typ == 'playingtime': typ = 'playing_time'
+            if typ == 'keepers': typ = 'keeper'
+            if typ == 'keepersadv': typ = 'keeper_adv'
+            print("le type : "+typ)
+            elem = browser.find_element(
+            By.XPATH, '//*[@id="all_stats_'+typ+'"]/div[1]/div/ul/li[1]')
+            elem.click()
+            WebDriverWait(browser, 10).until(EC.element_to_be_clickable((By.XPATH, '//*[@id="all_stats_'+typ+'"]/div[1]/div/ul/li[1]/div/ul/li[4]/button'))).click()
+            csv = browser.find_element(By.XPATH, '//*[@id="csv_stats_'+typ+'"]')
+            response = csv.text
+            response = response.replace(",", ";")
+            if typ == "shooting":
+             response = response.split("\n",1)[1]
+            else : response = response.split("\n",2)[2]
+            browser.close()
+            browser.quit()
+            key = league+'-'+typ 
+            data[key] = response
+    return data    
 
 app.run(port=5000)
